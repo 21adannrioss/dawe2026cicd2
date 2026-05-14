43dd7beed45d535a926e6ba8b1a00cb522fb59b7
Adan El Ouadah De Rios <166522477+21adannrioss@users.noreply.github.com>
2026-05-14 21:17:04 +0000

    deploy docker hub

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-// Conexió a la base de dades
+// Connexió a la base de dades
 $host = getenv('DB_HOST') ?: 'db';
 $dbname = getenv('DB_NAME') ?: 'taskmanager';
 $user = getenv('DB_USER') ?: 'taskuser';
